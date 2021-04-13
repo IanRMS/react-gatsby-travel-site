868c1393e17d93cd4e2c72577c8056b24be24d78
IanCorebiz <ian.ribeiro@corebiz.ag>
2021-04-13 16:56:11 -0300

    initial configuration

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Gatsby Travel Site`,
+    description: `Creating a website in order do study react and Gatsby.`,
+    author: `IanRMS`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
